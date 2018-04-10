60c6fc21d3596a8e6fca1de14d08821e7afa17bf
sreouven <sreouven@MacBook-Pro-de-Debo.local>
2018-04-10 21:22:49 +0200

    working clean

--- includes/libft.h
+++ includes/libft.h
@@ -84,5 +84,8 @@ void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+char				*ft_lltoa_base(unsigned long long n, int base);
+char				*ft_str_lower(char *str);
+char				*ft_str_upper(char *str);
 
 #endif
